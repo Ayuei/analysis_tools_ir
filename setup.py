cca236acca6bfa595406839812cca31877a20998
Ayuei <sefton_student@hotmail.com>
2022-04-29 15:24:29 +1000

    Added setup.py, refactoring into library code

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='analysis_tools_ir',
+    packages=find_packages(include=["analysis_tools_ir"]),
+    version='0.1.0',
+    description='Set of standard analysis tools for Information Retrieval',
+    author='Ayuei',
+    license='MIT',
+)
